9799db1f1f89add75d7a128e420817605f72888e
Tyler-Culp <tculp@ucsd.edu>
2022-09-15 13:19:12 -0700

    Update MineSweeper.java

--- MineSweeper.java
+++ MineSweeper.java
@@ -81,6 +81,66 @@ public class MineSweeper {
         }
 
     }
+
+    public void displayVisible()
+    {
+        System.out.print("\t ");
+        for(int i=0; i<10; i++)
+        {
+            System.out.print(" " + i + "  ");
+        }
+        System.out.print("\n");
+        for(int i=0; i<10; i++)
+        {
+            System.out.print(i + "\t| ");
+            for(int j=0; j<10; j++)
+            {
+                if(visible[i][j]==0)
+                {
+                    System.out.print("?");
+                }
+                else if(visible[i][j]==50)
+                {
+                    System.out.print(" ");
+                }
+                else
+                {
+                    System.out.print(visible[i][j]);
+                }
+                System.out.print(" | ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public boolean playMove(){
+        Scanner scnr = new Scanner(System.in);
+        System.out.print("\nEnter Row Number: ");
+        int i = scnr.nextInt();
+        System.out.print("Enter Column Number: ");
+        int j = scnr.nextInt();
+
+        if (i < 0 || i > 9 || j < 0 || j > 9){
+            System.out.print("\nIncorrect Input!!");
+            return true;
+        }
+
+        if (hidden[i][j] == 100){
+            displayHidden();
+            System.out.print("Oops! You stepped on a mine!\n============GAME OVER============");
+            return false;
+        }
+
+        else if (hidden[i][j] == 0){
+            fixVisible(i, j);
+        }
+
+        else{
+            fixNeighbors(i, j);
+        }
+
+        return true;
+    }
     public static void main(String[] args){
         MineSweeper M = new MineSweeper();
         M.startGame();
